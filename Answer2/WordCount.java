813109910ab1ce0e647af4256e9d493ffdcb7bdf
Prashant <mishraprashant13@gmail.com>
2026-02-14 11:06:40 +0000

    answer 5

--- Answer2/WordCount.java
+++ Answer2/WordCount.java
@@ -1,6 +1,6 @@
 import java.io.IOException;
 import java.util.StringTokenizer;
-import org.apache.hadoop.io.LongWritable;
+
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.io.IntWritable;
@@ -11,27 +11,22 @@ import org.apache.hadoop.mapreduce.Reducer;
 import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 
-
 public class WordCount {
 
   public static class TokenizerMapper
-       extends Mapper<LongWritable, Text, Text, IntWritable>{
+       extends Mapper<Object, Text, Text, IntWritable>{
 
     private final static IntWritable one = new IntWritable(1);
     private Text word = new Text();
 
-    public void map(LongWritable key, Text value, Context context) 
-	throws IOException, InterruptedException {
-	String line = value.toString()
-		.replaceAll("[^a-zA-Z0-9\\s]", "")
-		.toLowerCase();
-    	StringTokenizer tokenizer = new StringTokenizer(line);
-	while (tokenizer.hasMoreTokens()) {
-      	word.set(tokenizer.nextToken());
-      	context.write(word, one);
-    	}
+    public void map(Object key, Text value, Context context
+                    ) throws IOException, InterruptedException {
+      StringTokenizer itr = new StringTokenizer(value.toString());
+      while (itr.hasMoreTokens()) {
+        word.set(itr.nextToken());
+        context.write(word, one);
       }
-    
+    }
   }
 
   public static class IntSumReducer
@@ -41,12 +36,13 @@ public class WordCount {
     public void reduce(Text key, Iterable<IntWritable> values,
                        Context context
                        ) throws IOException, InterruptedException {
-	int sum = 0;
-        for (IntWritable val : values) {
-            sum += val.get();
-        }
-        context.write(key, new IntWritable(sum));      
+      int sum = 0;
+      for (IntWritable val : values) {
+        sum += val.get();
       }
+      result.set(sum);
+      context.write(key, result);
+    }
   }
 
   public static void main(String[] args) throws Exception {
@@ -61,7 +57,6 @@ public class WordCount {
     FileInputFormat.addInputPath(job, new Path(args[0]));
     FileOutputFormat.setOutputPath(job, new Path(args[1]));
     System.exit(job.waitForCompletion(true) ? 0 : 1);
-
   }
 }
 
